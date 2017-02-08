34dea5812e6e358f898390f26b028a541637f2ec
mdaldhi <muhammaddwialdhi@gmail.com>
2017-02-08 13:49:44 +0700

    Query update, userform, usermanager

--- FinalProject2017/src/user/UserManager.java
+++ FinalProject2017/src/user/UserManager.java
@@ -1,7 +1,14 @@
 package user;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+
 public class UserManager {
-	public void changePassword(String input){
-		
+	public void changePassword(String input) throws SQLException{
+		SqlMapClient smc = utils.IbatisHelper.getSqlMapInstance();
+		smc.update("user.changePassword", input);
 	}
 }
