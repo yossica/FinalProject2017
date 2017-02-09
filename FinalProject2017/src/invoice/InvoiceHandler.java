0938428ad9fec4735317ca78eeeb1fcd7842e592
meldri <meldri.asan@yahoo.co.id>
2017-02-09 17:02:14 +0700

    invoice Tarik Data

--- FinalProject2017/src/invoice/InvoiceHandler.java
+++ FinalProject2017/src/invoice/InvoiceHandler.java
@@ -1,5 +1,7 @@
 package invoice;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,13 +10,20 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
+import client.ClientForm;
+import client.ClientManager;
+
 public class InvoiceHandler extends Action{
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		InvoiceForm invForm = (InvoiceForm) form;
-		invForm.setMessage("testing from invoice handler");
+		InvoiceForm invoiceForm = (InvoiceForm) form;
+		InvoiceManager invoiceManager = new InvoiceManager();
+		ClientManager clientManager = new ClientManager();
+		
+		List clientList=clientManager.getAll();
+		invoiceForm.setClientList(clientList);
 		return mapping.findForward("invoice");
 	}
 
