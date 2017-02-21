caa8ba3e6c57f39b7e825da650db4741842e7c2d
huangellys <huang.ellys@gmail.com>
2017-02-21 13:10:04 +0700

    resolve conflict

--- FinalProject2017/src/invoice/InvoiceDetailBean.java
+++ FinalProject2017/src/invoice/InvoiceDetailBean.java
@@ -44,12 +44,6 @@ public class InvoiceDetailBean implements Serializable {
 	public void setNumb(Integer numb) {
 		this.numb = numb;
 	}
-	public Double getFee() {
-		return fee;
-	}
-	public void setFee(Double fee) {
-		this.fee = fee;
-	}
 	public Double getTotalFee() {
 		return totalFee;
 	}
@@ -70,12 +64,6 @@ public class InvoiceDetailBean implements Serializable {
 	}
 	public void setEmployeeName(String employeeName) {
 		this.employeeName = employeeName;
-	}
-	public int getWorkDays() {
-		return workDays;
-	}
-	public void setWorkDays(int workDays) {
-		this.workDays = workDays;
 	}
 	public int getManDays() {
 		return manDays;
@@ -113,7 +101,7 @@ public class InvoiceDetailBean implements Serializable {
 	public void setChangedDate(String changedDate) {
 		this.changedDate = changedDate;
 	}
-<<<<<<< HEAD
+
 	public int getWorkDays() {
 		return workDays;
 	}
@@ -130,6 +118,4 @@ public class InvoiceDetailBean implements Serializable {
 	public void setFee(Double fee) {
 		this.fee = fee;
 	}
-=======
->>>>>>> refs/remotes/origin/master
 }
