7eb6ff606ccc0a47b7eb2e7a94fd6f0032d24cba
grace.yossica <grace.yossica@gmail.com>
2017-02-24 09:27:35 +0700

    invoice
    tombol back,
    read only desc

--- FinalProject2017/src/invoice/InvoiceHandler.java
+++ FinalProject2017/src/invoice/InvoiceHandler.java
@@ -443,6 +443,9 @@ public class InvoiceHandler extends Action {
 					invoiceDetailBean.setWorkDays(temp.getWorkDays());
 					invoiceForm.getProfessionalServiceList().add(invoiceDetailBean);
 				}
+				invoiceForm.setTransactionInvoiceHeaderId(invoiceForm.getInvoiceBean().getTransactionInvoiceHeaderId());
+				invoiceForm.setClientId(String.valueOf(invoiceForm.getInvoiceBean().getClientId()));
+				invoiceForm.setStatusInvoiceId(String.valueOf(invoiceForm.getInvoiceBean().getStatusInvoiceId()));
 				invoiceForm.getInvoiceBean().setDetailSize(String.valueOf(invoiceForm.getProfessionalServiceList().size()));
 				invoiceForm.setTask("editInvoice");
 				return  mapping.findForward("formInvoicePS");
@@ -505,6 +508,11 @@ public class InvoiceHandler extends Action {
 					TrainingBean trainingBean = trainingManager.getTrainingByInvoiceSettlementId(invoiceForm.getInvoiceBean().getTransactionInvoiceHeaderId());
 					invoiceForm.setTrainingBean(trainingBean);
 					invoiceForm.setDetailTrainingList(trainingManager.getDetailByIdHeader(trainingBean.getTransactionTrainingHeaderId()));					
+
+					invoiceForm.setTransactionInvoiceHeaderId(invoiceForm.getInvoiceBean().getTransactionInvoiceHeaderId());
+					invoiceForm.setClientId(String.valueOf(invoiceForm.getInvoiceBean().getClientId()));
+					invoiceForm.setStatusInvoiceId(String.valueOf(invoiceForm.getInvoiceBean().getStatusInvoiceId()));
+					invoiceForm.getInvoiceBean().setDetailSize(String.valueOf(invoiceForm.getProfessionalServiceList().size()));
 					return mapping.findForward("formInvoiceTRST");
 				}
 			}
